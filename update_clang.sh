ee647b3bbbf1ab3f1388fa42bdf4a85405ac5f3d
Kendall Harter <kendallremna@protonmail.com>
2024-08-08 04:58:46 -0700

    update_clang.sh actually updates now

--- update_clang.sh
+++ update_clang.sh
@@ -6,6 +6,7 @@ if ! [ -f "./tools/bin/clang++" ]; then
    echo "Local Clang installation doesn't seem to exist." 1>&2
 else
    cd tools/clang-p2996
+   git fetch --depth 1 origin p2996
    git checkout FETCH_HEAD
    cmake --build build --target install
 fi
