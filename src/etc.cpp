c43734e1d05e632a1e6fabc7f91fcf7454264597
Kendall Harter <kendallremna@protonmail.com>
2025-06-03 22:59:17 -0700

    Named variant start

--- src/etc.cpp
+++ src/etc.cpp
@@ -3,8 +3,96 @@
 #include <experimental/meta>
 #include <print>
 #include <ranges>
+#include <stdexcept>
 #include <string>
 #include <tuple>
+
+template<fixed_string Name, typename T>
+struct named_pair_struct {
+   inline static constexpr auto name = Name;
+   using type = T;
+};
+
+template<fixed_string Name, typename T>
+inline constexpr auto named_pair = named_pair_struct<Name, T>{};
+
+// The requires expression is a limitation of the compiler - in C++26 trivial unions are a thing
+template<named_pair_struct... Members>
+   requires(std::is_trivially_destructible_v<typename decltype(Members)::type> && ...)
+struct named_variant {
+private:
+   union storage_union;
+   consteval
+   {
+      std::meta::define_aggregate(
+         ^^storage_union, {std::meta::data_member_spec(^^typename decltype(Members)::type)...});
+   }
+
+   int index_;
+   storage_union storage_;
+
+   static consteval auto get_nth_member(std::size_t i) -> std::meta::info
+   {
+      const auto ctx = std::meta::access_context::current();
+      return std::meta::nonstatic_data_members_of(^^storage_union, ctx)[i];
+   }
+
+   template<std::size_t I, typename T>
+   constexpr named_variant(std::integral_constant<std::size_t, I>, T&& value)
+      requires std::convertible_to<decltype(value), [ : std::meta::type_of(get_nth_member(I)) : ]> {
+         storage_.[:get_nth_member(I):] = std::forward<T>(value);
+         index_ = I;
+      }
+
+   template<fixed_string Name>
+   static consteval auto get_index_by_name()
+      -> std::size_t{template for (constexpr auto i : ::define_static_array(std::views::iota(0zu, sizeof...(Members)))){
+         if (Name == Members...[i].name){return static_cast<std::size_t>(i);
+}
+}
+throw std::runtime_error{"Name not found"};
+}
+
+public:
+template<fixed_string Name, typename T>
+constexpr static named_variant create(T&& val)
+{
+   constexpr auto index = get_index_by_name<Name>();
+   return {std::integral_constant<std::size_t, index>{}, std::forward<T>(val)};
+}
+
+constexpr ~named_variant()
+{
+   template for (constexpr auto i : ::define_static_array(std::views::iota(0zu, sizeof...(Members))))
+   {
+      if (i == index_) {
+         std::destroy_at(&storage_.[:get_nth_member(i):]);
+         return;
+      }
+   }
+}
+
+template<fixed_string Name>
+constexpr auto get() const& noexcept -> const decltype(storage_.[:get_nth_member(get_index_by_name<Name>()):])*
+{
+   constexpr auto index = get_index_by_name<Name>();
+   if (index != index_) {
+      return nullptr;
+   }
+   return &storage_.[:get_nth_member(index):];
+}
+
+template<fixed_string Name>
+constexpr auto get() & noexcept -> decltype(storage_.[:get_nth_member(get_index_by_name<Name>()):])*
+{
+   constexpr auto index = get_index_by_name<Name>();
+   if (index != index_) {
+      return nullptr;
+   }
+   return &storage_.[:get_nth_member(index):];
+}
+}
+;
 
 template<typename T>
 struct debug_print_struct {
@@ -116,4 +204,8 @@ int main()
    const auto test = hoot{.x = 20};
    std::print("{}\n", as_debug(hoot{.x = 20}));
    std::print("{}\n", as_debug(std::tuple{1, 2, 3, 4, 5}));
+   using var = named_variant<named_pair<"wow", int>, named_pair<"wow2", int>>;
+   constexpr var a = var::create<"wow">(10);
+   static_assert(!a.get<"wow2">());
+   static_assert(*a.get<"wow">() == 10);
 }
